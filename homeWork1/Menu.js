137dabf94524d9c6c6fedb020572f5ca32f48eac
AlexeyKobevko <folco_2000@mail.ru>
2018-12-23 14:10:45 +0300

    1st

---
+++ homeWork1/Menu.js
@@ -0,0 +1,21 @@
+class Menu {
+    constructor(id, className, items){
+        this.id = id;
+        this.className = className;
+        this.items = items;
+    }
+    render(){
+        let result = `<ul class="${this.className}" id="${this.id}">`;
+        for (let i = 0; i < this.items.length; i++){
+            if (this.items[i] instanceof MenuItem){
+                result += this.items[i].render();
+            }
+        }
+        result += '</ul>';
+        return result;
+    }
+    remove(){
+        let delMenu = document.getElementById(this.className);
+        delMenu.remove();
+    }
+}
