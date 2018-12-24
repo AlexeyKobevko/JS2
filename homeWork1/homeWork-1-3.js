9f5f999dfc6030cb58eec5da2d72c06a672d4fa2
AlexeyKobevko <folco_2000@mail.ru>
2018-12-24 13:51:30 +0300

    исправил ошибки

--- homeWork1/homeWork-1-3.js
+++ homeWork1/homeWork-1-3.js
@@ -1,3 +1,4 @@
+'use strict';
 class Hamburger {
   constructor (size, filling, flavoring, mayo) {
     this.size = size;
@@ -7,37 +8,29 @@ class Hamburger {
   }
   getPrice() {
     let result = 0;
-    if (this.size === 'small') {
-      result += 50;
-    } else result += 100;
+
+    if (this.size === 'small') result += 50;
+      else if (this.size === 'big') result += 100;
     
-    if (this.filling === 'cheese') {
-      result += 10;
-    } else if (this.filling === 'salad') {
-      result += 20;
-    } else if (this.filling === 'potato') {
-      result += 15;
-    }
+    if (this.filling === 'cheese') result += 10;
+      else if (this.filling === 'salad') result += 20;
+      else if (this.filling === 'potato') result += 15;
 
     if (this.flavoring) result += 15;
     if (this.mayo) result += 20;
 
-    return result;
+  return result;
   }
 
   getCalories() {
     let result = 0;
-    if (this.size === 'small') {
-      result += 20;
-    } else result += 40;
+    if (this.size === 'small') result += 20;
+      else result += 40;
 
-    if (this.filling === 'cheese') {
-      result += 20;
-    } else if (this.filling === 'salad') {
-      result += 5;
-    } else if (this.filling === 'potato') {
-      result += 10;
-    }
+    if (this.filling === 'cheese') result += 20;
+      else if (this.filling === 'salad') result += 5;
+      else if (this.filling === 'potato') result += 10;
+
 
     if (this.mayo) result += 5;
 
