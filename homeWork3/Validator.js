a26ef3841b2e4d28e65b0d64d99f58d5dc3fcfc7
djedaj <kobevko@gmail.com>
2019-01-10 19:25:44 +0300

    start Validator

--- homeWork3/Validator.js
+++ homeWork3/Validator.js
@@ -1,15 +1,55 @@
 class Validator {
-    constructor(id, pattern) {
-        this.id = id;
-        this.pattern = pattern;
+    constructor(idName, idPhone, idEmail, idText, successStyle, failStyle) {
+        this.idName = idName;
+        this.idPhone = idPhone;
+        this.idEmail = idEmail;
+        this.idText = idText;
+        this.successStyle = successStyle;
+        this.failStyle = failStyle;
     }
 
-    getValue(){
-        let variable = document.getElementById(`${this.id}`).value;
-        if(this.pattern.test(variable)){
-            document.getElementById(`${this.id}`).classList.add('not-error');
+    _regExp = {
+        name: '/a-zаяё/i',
+        phone: '/\+\d\(\d{3}\)\d{3}-\d{4}/',
+        email: '/[a-z]+[.-]?[a-z]+?@[a-z]+\.ru/',
+        text: '/\w{80}/gm',
+    }
+    
+    isName() {
+        let nameUser = document.getElementById(this.idName).value;
+        if (this._regExp.name.test(nameUser)) {
+            document.getElementById(this.idName).classList.add(this.successStyle);
         } else {
-            document.getElementById(`${this.id}`).classList.add('error');
+            document.getElementById(this.idName).classList.add(this.failStyle);
         }
     }
+
+    isPhone() {
+        let phoneUser = document.getElementById(this.idPhone).value;
+        if (this._regExp.phone.test(phoneUser)) {
+            document.getElementById(this.idPhone).classList.add(this.successStyle);
+        } else {
+            document.getElementById(this.idPhone).classList.add(this.failStyle);
+        }
+    }
+
+    isEmail() {
+        let emailUser = document.getElementById(this.idEmail).value;
+        if (this._regExp.phone.test(emailUser)) {
+            document.getElementById(this.idEmail).classList.add(this.successStyle);
+        } else {
+            document.getElementById(this.idEmail).classList.add(this.failStyle);
+        }
+    }
+
+    isText() {
+        let textUser = document.getElementById(this.idText).value;
+        if (this._regExp.text.test(textUser)) {
+            document.getElementById(this.idText).classList.add(this.successStyle);
+        } else {
+            document.getElementById(this.idText).classList.add(this.failStyle);
+        }
+    }
+
+
 }
