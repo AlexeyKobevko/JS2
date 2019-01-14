b4b52b233fdd88a3ec82e3dbf598e618bd31762e
Alexey <kobevko@gmail.com>
2019-01-14 21:45:06 +0300

    ready to task2

--- homeWork3/Validator.js
+++ homeWork3/Validator.js
@@ -1,34 +1,64 @@
 class Validator {
-    constructor(idName, idPhone, idEmail) {
-        this.idName = this._noda(idName);
-        this.idPhone = this._noda(idPhone);
-        this.idEmail = this._noda(idEmail);
+    constructor(form, errorClass = 'error-msg') {
+        this.patterns = {
+            name: /^[a-zа-яА-ЯЁё]+$/i,
+            phone: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
+            email: /^[\w._-]+@\w+\.\w{2,4}$/i
+        };
+        this.errors = {
+            name: 'Имя содержит только буквы',
+            phone: 'Телефон подчиняется шаблону +7(000)000-0000',
+            email: 'E-mail выглядит как mymail@mail.ru, или my.mail@mail.ru, или my-mail@mail.ru'
+        };
+        this.errorClass = errorClass;
+        this.form = form;
+        this.valid = false;
+        this._validateForm(this.form);
     }
-
-    // присваиваю каждому ID ноду
-
-    _noda(id) {
-        let elem = document.getElementById(id);
-        return elem;
+    _validateForm(form){
+        let errors = [...document.getElementById(form).querySelectorAll(`.${this.errorClass}`)];
+        console.log(errors);
+        for (let error of errors){
+            error.remove();
+        }
+        let formFields = [...document.getElementById(form).getElementsByTagName('input')];
+        for (let field of formFields){
+            this._validate(field);
+        }
+        if(![...document.querySelectorAll('.invalid')].length){
+            this.valid = true;
+        }
     }
-
-    _regExp = {
-        name: /^[a-zа-яё]+$/i,
-        phone: /^\+\d\(\d{3}\)\d{3}-\d{4}$/,
-        email: /^[a-z]+[.-]?[a-z]+?@[a-z]+\.ru$/
-    };
-
-    // Выясняю валидно ли поле инпут
-
-    _isValid(noda, regExp) {
-        return regExp.test(noda.value);
+    _validate(field){
+        if(this.patterns[field.name]){
+            if(!this.patterns[field.name].test(field.value)){
+               field.classList.add('invalid');
+               this._addErrorMsg(field);
+               this._watchField(field);
+            }
+        }
     }
-
-    // добавляю класс в инпут
-
-    setClass() {
-        if (this._isValid(this.idName, this._regExp.name)) {
-            this.idName.classList.add('not-error');
-        } else this.idName.classList.add('error');
+    _addErrorMsg(field){
+        let errMsg = document.createElement('div');
+        errMsg.classList.add(this.errorClass);
+        errMsg.textContent = this.errors[field.name];
+        field.parentNode.appendChild(errMsg);
+    }
+    _watchField(field){
+        field.addEventListener('input', () => {
+            if(this.patterns[field.name].test(field.value)){
+                field.classList.remove('invalid');
+                field.classList.add('valid');
+                if(field.parentNode.lastChild !== field){
+                    field.parentNode.lastChild.remove();
+                }
+            } else {
+                field.classList.remove('valid');
+                field.classList.add('invalid');
+                if (field.parentNode.lastChild.nodeName !== 'DIV'){
+                    this._addErrorMsg(field);
+                }
+            }
+        })
     }
 }
